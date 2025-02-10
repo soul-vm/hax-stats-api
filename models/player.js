391805f12a68d54b71f5cec22a77d42affbbdf4a
Axel Vera <83617294+soul-vm@users.noreply.github.com>
2025-02-10 17:36:42 -0300

    Modifica el modelo player
    - Agrega country
    - Elimina position y team

--- models/player.js
+++ models/player.js
@@ -3,10 +3,9 @@ import mongoose from "mongoose";
 const playerSchema = new mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
+  country: { type: String, required: true, maxlength: 2 },
   nickname: { type: String, required: true },
   avatar: { type: String, required: true, maxlength: 2 },
-  position: { type: String, required: true },
-  team: { type: String },
   stats: {
     goals: { type: Number, default: 0 },
     assists: { type: Number, default: 0 },
