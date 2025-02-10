d47af9c45367c85d27a7b160bc8ca82bd088ca85
Axel Vera <83617294+soul-vm@users.noreply.github.com>
2025-02-10 17:35:40 -0300

    Agrega CORS

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import connectToDatabase from "./config/database.js";
 import playerRoutes from "./routes/playerRoutes.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
 app.use("/players", playerRoutes);
 
 connectToDatabase();
