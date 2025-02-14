40584036d779c4015ec821ad6c3c1989063adb33
Axel Vera <83617294+soul-vm@users.noreply.github.com>
2025-02-14 01:47:34 -0300

    Implementa bcrypt en register y login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,8 +1,18 @@
+import bcrypt from "bcryptjs";
+
 import Player from "../models/player.js";
 
 const register = async (req, res) => {
   try {
-    const player = new Player(req.body);
+    const { email, password, country, nickname, avatar } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const player = new Player({
+      email,
+      password: hashedPassword,
+      country,
+      nickname,
+      avatar,
+    });
     await player.save();
     res.status(201).json(player);
   } catch (error) {
@@ -12,15 +22,15 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
   try {
-    const player = await Player.findOne({
-      email: req.body.email,
-      password: req.body.password,
-    });
-    if (!player) {
-      res.status(404).json({ message: "Usuario no encontrado" });
-    } else {
-      res.status(200).json(player._id);
-    }
+    const player = await Player.findOne({ email: req.body.email });
+    if (!player)
+      return res.status(404).json({ message: "Usuario no encontrado" });
+
+    const isMatch = await bcrypt.compare(req.body.password, player.password);
+    if (!isMatch)
+      return res.status(401).json({ message: "Credenciales incorrectas" });
+
+    res.status(200).json(player._id);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
